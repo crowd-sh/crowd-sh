e1b3a694e36ceb9e5c967dcf7406d791f929a3d0
Abhi Yerra <abhi.yerra@lookout.com>
2014-01-02 01:56:51 -0800

    A Job writer interface.

---
+++ app/writer.go
@@ -0,0 +1,32 @@
+package workmachine
+
+import (
+	"encoding/csv"
+	"fmt"
+	"os"
+)
+
+func CsvJobWriter(file *os.File) func(j *Job) {
+	writer := csv.NewWriter(file)
+
+	return func(j *Job) {
+		fmt.Printf("%v\n", j)
+
+		var output []string
+
+		for _, i := range j.InputFields {
+			output = append(output, i.Value)
+			fmt.Println(i.Value)
+		}
+
+		for _, i := range j.OutputFields {
+			output = append(output, i.Value)
+			fmt.Println(i.Value)
+		}
+
+		if err := writer.Write(output); err != nil {
+			panic(err)
+		}
+		writer.Flush()
+	}
+}
